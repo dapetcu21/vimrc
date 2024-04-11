92a56f908b5bd1ba84e157c767d4c73bc4bb0160
Marius Petcu <marius@petcu.me>
2024-04-11 16:01:02 +0300

    Add glob edit prompt

--- lua/userconf/plugins/fzf.lua
+++ lua/userconf/plugins/fzf.lua
@@ -1,10 +1,26 @@
+vim.g.fzf_glob = vim.g.fzf_glob or ""
+
+local function set_glob()
+  vim.ui.input({ prompt = 'Grep Filter Globs>', default = vim.g.fzf_glob }, function (input)
+    if input ~= nil then
+      vim.g.fzf_glob = input
+    end
+  end)
+end
+
 local function grep_with_confirm(default_search)
-  vim.ui.input({ prompt = 'Grep For>', default = default_search }, function (search)
+  local glob = vim.g.fzf_glob or ""
+  local prompt = (glob == "") and "Grep For>" or ("Grep For (" .. glob .. ")>")
+  vim.ui.input({ prompt = prompt, default = default_search }, function (search)
     if search == nil then
       return
     end
-    require('fzf-lua').grep({ search = search })
+    require('fzf-lua').grep({ search = (glob == "") and search or (search .. " -- " .. glob)})
   end)
+end
+
+local function grep()
+  grep_with_confirm()
 end
 
 local function grep_visual()
@@ -31,7 +47,8 @@ return {
     keys = {
       { "<space>/", grep_visual, mode = 'v', silent = true },
       { "<space>/", grep_cword, mode = 'n', silent = true },
-      { "<space>?", "<Cmd>FzfLua grep<CR>", mode = 'n', silent = true },
+      { "<space>?", grep, mode = 'n', silent = true },
+      { "<C-/>", set_glob, mode = 'n', silent = true },
       { "<space>z", "<Cmd>FzfLua<CR>", mode = 'n', silent = true },
       { "<space>f", "<Cmd>FzfLua files<CR>", mode = 'n', silent = true },
       { "<space>F", "<Cmd>FzfLua oldfiles<CR>", mode = 'n', silent = true },
