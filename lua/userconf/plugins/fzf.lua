bba12909e387d2b46a1f0d87104395020293ee86
Marius Petcu <marius@petcu.me>
2024-04-11 12:08:35 +0300

    Add opportunity to edit prompt for fzf <space>/

--- lua/userconf/plugins/fzf.lua
+++ lua/userconf/plugins/fzf.lua
@@ -1,3 +1,24 @@
+local function grep_with_confirm(default_search)
+  vim.ui.input({ prompt = 'Grep For>', default = default_search }, function (search)
+    if search == nil then
+      return
+    end
+    require('fzf-lua').grep({ search = search })
+  end)
+end
+
+local function grep_visual()
+  local utils = require "fzf-lua.utils"
+  local search = utils.get_visual_selection()
+  grep_with_confirm(search)
+end
+
+local function grep_cword()
+  local utils = require "fzf-lua.utils"
+  local search = [[\b]] .. utils.rg_escape(vim.fn.expand("<cword>")) .. [[\b]]
+  grep_with_confirm(search)
+end
+
 return {
   {
     "ibhagwan/fzf-lua",
@@ -8,8 +29,9 @@ return {
 
     cmd = { 'FzfLua' },
     keys = {
-      { "<space>/", "<Cmd>FzfLua grep_visual<CR>", mode = 'v', silent = true },
-      { "<space>/", "<Cmd>FzfLua grep_cword<CR>", mode = 'n', silent = true },
+      { "<space>/", grep_visual, mode = 'v', silent = true },
+      { "<space>/", grep_cword, mode = 'n', silent = true },
+      { "<space>?", "<Cmd>FzfLua grep<CR>", mode = 'n', silent = true },
       { "<space>z", "<Cmd>FzfLua<CR>", mode = 'n', silent = true },
       { "<space>f", "<Cmd>FzfLua files<CR>", mode = 'n', silent = true },
       { "<space>F", "<Cmd>FzfLua oldfiles<CR>", mode = 'n', silent = true },
