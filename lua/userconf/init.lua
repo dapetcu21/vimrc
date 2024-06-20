ee3b894872a6e87e5736a68527f41c51cdcfd602
Marius Petcu <marius@petcu.me>
2024-06-20 11:28:10 +0300

    Add copy current file path commands

--- lua/userconf/init.lua
+++ lua/userconf/init.lua
@@ -13,3 +13,18 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup('userconf.plugins')
+
+local function copy_path(absolute)
+  local path = vim.fs.normalize(vim.fn.expand(absolute and '%:p' or '%:.'));
+
+  if vim.loop.os_uname().sysname:find('Windows') then
+    path = path:gsub('/', '\\')
+  end
+
+  vim.fn.setreg('+', path)
+
+  vim.notify(path, vim.log.levels.INFO)
+end
+
+vim.api.nvim_create_user_command('CopyPath', function () copy_path(false) end, {})
+vim.api.nvim_create_user_command('CopyFullPath', function () copy_path(true) end, {})
