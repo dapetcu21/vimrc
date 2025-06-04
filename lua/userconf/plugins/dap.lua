90f9577cdcea927d1b66b1a6dc52dc6d469cf79b
Marius Petcu <marius@petcu.me>
2025-06-04 15:40:59 +0300

    Correctly bind dapui events

--- lua/userconf/plugins/dap.lua
+++ lua/userconf/plugins/dap.lua
@@ -66,17 +66,7 @@ return {
     },
 
     config = function()
-      local dap = require("dap")
-
-      dap.listeners.after.event_initialized["dapui_config"] = function()
-        require("dapui").open()
-      end
-      dap.listeners.before.event_terminated["dapui_config"] = function()
-        require("dapui").close()
-      end
-      dap.listeners.before.event_exited["dapui_config"] = function()
-        require("dapui").close()
-      end
+      vim.fn.sign_define("DapBreakpoint", { text = "🐞" });
     end,
   },
   {
@@ -97,8 +87,22 @@ return {
     },
 
     config = function ()
-      require("dapui").setup()
-      vim.fn.sign_define("DapBreakpoint", { text = "🐞" });
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup()
+
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
     end
   },
 }
